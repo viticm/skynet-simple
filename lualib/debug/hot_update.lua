3f61b3999ce6c669ef1af8c0347ad4c022046a62
leafly <leafly.ti@hotmail.com>
2020-11-28 15:07:27 +0800

    Add the reload module and test passed

--- lualib/debug/hot_update.lua
+++ lualib/debug/hot_update.lua
@@ -1,3 +1,14 @@
+--[[
+ - SKYNET SIMPLE ( https://github.com/viticm/skynet-simple )
+ - $Id hot_update.lua
+ - @link https://github.com/viticm/skynet-simple for the canonical source repository
+ - @copyright Copyright (c) 2020 viticm( viticm.ti@gmail.com )
+ - @license
+ - @user viticm( viticm.ti@gmail.com )
+ - @date 2020/11/28 15:03
+ - @uses The lua file reload module.
+         !ref:https://github.com/asqbtcupid/lua_hotupdate
+--]]
 local lfs = require 'lfs'
 
 local getfenv, setfenv, loadstring
