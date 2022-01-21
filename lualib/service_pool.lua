fccd21818a8d0890be982d94d4d8a66c2529acdd
viticm <viticm@126.com>
2022-01-21 16:39:18 +0800

    Fix error

--- lualib/service_pool.lua
+++ lualib/service_pool.lua
@@ -19,6 +19,7 @@ local table = table
 local setmetatable = setmetatable
 local print = print
 local next = next
+local math = math
 
 -- Data.
 -------------------------------------------------------------------------------
